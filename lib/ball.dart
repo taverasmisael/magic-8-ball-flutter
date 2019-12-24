69021954363614c70941df138769c1a945c16808
Misael Taveras <macrot29@gmail.com>
2019-12-24 15:26:53 -0400

    feat: complete excercise

---
+++ lib/ball.dart
@@ -0,0 +1,25 @@
+import 'package:flutter/material.dart';
+import 'dart:math';
+
+class Ball extends StatefulWidget {
+  @override
+  _BallState createState() => _BallState();
+}
+
+class _BallState extends State<Ball> {
+  int ballNumber = 1;
+
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: FlatButton(
+        onPressed: () {
+          setState(() {
+            ballNumber = Random().nextInt(4) + 1;
+          });
+        },
+        child: Image.asset('images/ball$ballNumber.png'),
+      ),
+    );
+  }
+}
--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,26 @@
 import 'package:flutter/material.dart';
+import 'package:magic_8_ball/ball.dart';
 
 void main() => runApp(
       MaterialApp(
-        home: null,
+        home: BallPage(),
       ),
     );
+
+class BallPage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return SafeArea(
+      child: Scaffold(
+        backgroundColor: Colors.blue[400],
+        appBar: AppBar(
+          title: Text('Ask me anythihng'),
+          backgroundColor: Colors.blue[800],
+        ),
+        body: Container(
+          child: Ball(),
+        ),
+      ),
+    );
+  }
+}
